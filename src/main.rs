864ee09d7d143b81784178575d1e95914f3d7eef
Manuel Valladares Pernas <mavallad@gmail.com>
2018-08-01 19:38:20 +0200

    Add graphics and clear terminal in each turn

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+mod graphics;
+
 #[macro_use] extern crate text_io;
 use std::io::prelude::*;
 use std::io;
 use std::collections::HashSet;
 use std::env;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,20 +15,23 @@ fn main() {
         std::process::exit(1);
     }
     let secret = args[1].to_uppercase();
+    clear_console();
     play_game(secret);
 }
 
 fn play_game(secret: String) {
     let secret_letters = extract_letters(&secret);
     let mut finish = false;
-    let mut attempts_left = 8;
+    let mut attempts_left: u8 = 6;
     let mut letters_found: HashSet<char> = HashSet::new();
     while !finish {
         println!("INTENTOS: {}", attempts_left);
-        println!("{}", pattern_secret(&secret, &letters_found));
+        println!("{}", graphics::paint(attempts_left));
+        println!("\n      {}\n", pattern_secret(&secret, &letters_found));
         print!("Letra: ");
         io::stdout().flush().ok().expect("No podemos limpiar la salida");
         let rc: Result<char, _> = try_read!();
+        clear_console();
         match rc {
             Ok(c) if is_valid_letter(&c) => {
                 let new_char = uppercase(c);
@@ -33,12 +39,15 @@ fn play_game(secret: String) {
                     letters_found.insert(new_char);
                     if letters_found == secret_letters {
                         println!("CAMPEÓN: Has acertado la palabra secreta {}", secret);
+                        println!("{}", graphics::paint(attempts_left));
                         finish = true;
                     }
                 } else {
                     println!("ERROR. {} no está en la palabra", new_char);
                     attempts_left -= 1;
                     if attempts_left == 0 {
+                        println!("{}", "HAS PERDIDO!");
+                        println!("{}", graphics::paint(attempts_left));
                         println!("Has acabado tus intentos. La palabra secreta es: {}", secret);
                         finish = true;
                     }
@@ -79,4 +88,8 @@ fn uppercase(c: char) -> char {
     } else {
         c.to_ascii_uppercase()
     }
+}
+
+fn clear_console() {
+    print!("{}[2J", 27 as char);
 }
